6ebf8bd19e2469888f107307be0b23c7892e2b25
Tilman Holube <tilman@holube.de>
2026-03-25 17:47:26 +0100

    Study 2026

--- src/main/java/de/holube/fakestudy/configs/StudyFactory2026.java
+++ src/main/java/de/holube/fakestudy/configs/StudyFactory2026.java
@@ -37,130 +37,130 @@ public class StudyFactory2026 extends StudyFactory {
                 .decimalPlaces(0)
                 .build();
 
-        numberCat("D", "Geschmackssensitivität")
+        numberCat("D", "Body-Mass-Index")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(-1d)
                 .distribution(distribution("D",
-                        fromDiff(25, 5),
-                        fromDiff(90, 10),
+                        fromDiff(18, 3),
+                        fromDiff(45, 5),
                         otherDistributionType("C"),
-                        fromDiff(15, 5)
+                        fromDiff(8, 2)
                 ))
                 .decimalPlaces(1)
                 .build();
 
-        correlationCat("E", "Preferierter Kakaoanteil (in Prozent)", "C")
+        correlationCat("E", "Schokoladenkonsum pro Woche (in g)", "D")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(-1d)
                 .correlator(Correlators.NORMAL_CORRELATOR)
-                .min(10)
-                .max(95)
+                .min(0)
+                .max(1000)
                 .distribution(distribution("E",
-                        fromDiff(-30, 5),
-                        fromDiff(30, 5),
+                        fromDiff(-500, 50),
+                        fromDiff(500, 50),
                         0,
-                        fromDiff(10, 2)
+                        fromDiff(200, 20)
                 ))
                 .decimalPlaces(0)
                 .build();
 
-        selectionCat("F", "Anreise zu den Eisdielen")
+        selectionCat("F", "Präferenztyp")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue("u")
-                .selection("S", "F", "R", "A", "B")
+                .selection("B", "M", "W", "N", "K")
                 .min(10)
                 .max(100)
                 .build();
 
-        selectionCat("G", "Lieblings-Eissorte")
+        selectionCat("G", "Am häufigsten genutzter Lebensmittelhändler")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue("u")
-                .selection("S", "V", "Z", "E", "H", "M")
+                .selection("A", "C", "E", "F", "N", "R")
                 .min(10)
                 .max(100)
                 .build();
 
-        selectionCat("H", "Häufigkeit des Eisessens")
+        selectionCat("H", "Tabak- und/oder Alkoholkonsum")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue("u")
-                .selection("T", "W", "M", "S")
+                .selection("T", "W", "M", "N")
                 .min(10)
                 .max(100)
                 .build();
 
-        numberCat("I", "Bissfeste der Schokolade bei BAR (Einheit mBar)")
+        numberCat("I", "Kakaoanteil der Schokolade bei DIS (in %)")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(999d)
                 .distribution(distribution("I",
-                        fromDiff(-16, 2),
-                        fromDiff(-8, 2),
+                        fromDiff(7, 7),
+                        fromDiff(83, 7),
                         randomDistributionType(),
-                        fromDiff(2, 0.5)
+                        fromDiff(20, 5)
                 ))
                 .decimalPlaces(1)
                 .build();
 
-        numberCat("J", "Temperatur des Eises bei MANU (Einheit °C)")
+        numberCat("J", "Kakaoanteil der Schokolade bei MAR (in %)")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(999d)
                 .distribution(distribution("J",
-                        fromDiff(-16, 2),
-                        fromDiff(-8, 2),
+                        fromDiff(7, 7),
+                        fromDiff(83, 7),
                         randomDistributionType(),
-                        fromDiff(2, 0.5)
+                        fromDiff(20, 5)
                 ))
                 .decimalPlaces(1)
                 .build();
 
-        numberCat("K", "Temperatur des Eises bei CAFE (Einheit °C)")
+        numberCat("K", "Kakaoanteil der Schokolade bei PRE (in %)")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(999d)
                 .distribution(distribution("K",
-                        fromDiff(-16, 2),
-                        fromDiff(-8, 2),
+                        fromDiff(7, 7),
+                        fromDiff(83, 7),
                         randomDistributionType(),
-                        fromDiff(2, 0.5)
+                        fromDiff(20, 5)
                 ))
                 .decimalPlaces(1)
                 .build();
 
-        numberCat("L", "Gewicht der Schokolade bei BAR (Einheit g)")
+        numberCat("L", "Schmelzpunkt der Schokolade bei DIS (Einheit °C)")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(-1d)
                 .distribution(distribution("L",
-                        fromDiff(35, 5),
-                        fromDiff(140, 10),
+                        fromDiff(23, 3),
+                        fromDiff(52, 3),
                         otherDistributionType("I"),
-                        fromDiff(30, 5)
+                        fromDiff(10, 3)
                 ))
                 .decimalPlaces(0)
                 .build();
 
-        numberCat("M", "Gewicht einer Eiskugel bei MANU (Einheit g)")
+        numberCat("M", "Schmelzpunkt der Schokolade bei MAR (Einheit °C)")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(-1d)
                 .distribution(distribution("M",
-                        fromDiff(35, 5),
-                        fromDiff(140, 10),
+                        fromDiff(23, 3),
+                        fromDiff(52, 3),
                         otherDistributionType("J"),
-                        fromDiff(30, 5)
+                        fromDiff(10, 3)
                 ))
                 .decimalPlaces(0)
                 .build();
 
-        numberCat("N", "Gewicht einer Eiskugel bei CAFE (Einheit g)")
+        numberCat("N", "Schmelzpunkt der Schokolade bei PRE (Einheit °C)")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(-1d)
                 .distribution(distribution("N",
-                        fromDiff(35, 5),
-                        fromDiff(140, 10),
+                        fromDiff(23, 3),
+                        fromDiff(52, 3),
                         otherDistributionType("K"),
-                        fromDiff(30, 5)
+                        fromDiff(10, 3)
                 ))
                 .decimalPlaces(0)
                 .build();
 
-        numberCat("O", "Bewertung des Geschmacks bei BAR")
+        numberCat("O", "Bewertung des Geschmacks bei DIS")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(-1d)
                 .distribution(distribution("O",
@@ -172,7 +172,7 @@ public class StudyFactory2026 extends StudyFactory {
                 .decimalPlaces(0)
                 .build();
 
-        numberCat("P", "Bewertung des Geschmacks bei MANU")
+        numberCat("P", "Bewertung des Geschmacks bei MAR")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(-1d)
                 .distribution(distribution("P",
@@ -184,7 +184,7 @@ public class StudyFactory2026 extends StudyFactory {
                 .decimalPlaces(0)
                 .build();
 
-        numberCat("Q", "Bewertung des Geschmacks bei CAFE")
+        numberCat("Q", "Bewertung des Geschmacks bei PRE")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(-1d)
                 .distribution(distribution("Q",
@@ -196,7 +196,7 @@ public class StudyFactory2026 extends StudyFactory {
                 .decimalPlaces(0)
                 .build();
 
-        numberCat("R", "Bewertung der Beschaffenheit (Cremigkeit und Abschmelzverhalten) bei BAR")
+        numberCat("R", "Bewertung der Kaufbereitschaft bei DIS")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(-1d)
                 .distribution(distribution("R",
@@ -208,7 +208,7 @@ public class StudyFactory2026 extends StudyFactory {
                 .decimalPlaces(0)
                 .build();
 
-        numberCat("S", "Bewertung der Beschaffenheit (Cremigkeit und Abschmelzverhalten) bei MANU")
+        numberCat("S", "Bewertung der Kaufbereitschaft bei MAR")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(-1d)
                 .distribution(distribution("S",
@@ -220,7 +220,7 @@ public class StudyFactory2026 extends StudyFactory {
                 .decimalPlaces(0)
                 .build();
 
-        numberCat("T", "Bewertung der Beschaffenheit (Cremigkeit und Abschmelzverhalten) bei CAFE")
+        numberCat("T", "Bewertung der Kaufbereitschaft bei PRE")
                 .missingPercentage(fromDiff(defaultMissingBase, defaultMissingDiff).doubleValue())
                 .missingValue(-1d)
                 .distribution(distribution("T",
